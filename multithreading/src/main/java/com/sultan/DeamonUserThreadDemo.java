c9395eac565319466f2c101324a9983cac79fb37
sultan <sultanmalik1800@gmail.com>
2024-10-15 23:06:00 +0530

    basics of multi threading

---
+++ multithreading/src/main/java/com/sultan/DeamonUserThreadDemo.java
@@ -0,0 +1,46 @@
+package com.sultan;
+
+public class DeamonUserThreadDemo {
+    public static void main(String[] args) {
+        Thread bgThread = new Thread(new DeamonHelper());
+        Thread userThread = new Thread(new UserThreadHelper());
+        bgThread.setDaemon(true);
+
+        bgThread.start();
+        userThread.start();
+    }
+}
+
+class DeamonHelper implements Runnable {
+    @Override
+    public void run() {
+        int count = 0;
+        while (count < 500) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            count++;
+            System.out.println("Deamon helper running...");
+        }
+    }
+}
+
+class UserThreadHelper implements Runnable {
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println("User thread done with execution...");
+    }
+}
+
+// deamon threads automatically get completed a soon as all
+// user threads complete their execution
